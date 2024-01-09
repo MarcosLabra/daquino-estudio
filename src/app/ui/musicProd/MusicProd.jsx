60f0ff78b34f1b2596865a737b4a0f3683762045
MarcosLabra <mlabra.dev@gmail.com>
2024-01-09 16:30:04 -0300

    hago responsive de netbook en  toda la pag salvo carrousel y footer

--- src/app/ui/musicProd/MusicProd.jsx
+++ src/app/ui/musicProd/MusicProd.jsx
@@ -8,26 +8,42 @@ export default function MusicProd() {
     <section id="musicProd" className={style.musicProd}>
       <SectionTitle title={"Produccion Musical"} />
       <article>
-        <p>En la producción musical, el viaje creativo es tan crucial como el resultado final. Aunque alcanzar un resultado excepcional es el objetivo, el proceso es donde se despliega la pasión y la expresión artística. Cada fase, desde la concepción hasta la ejecución, aporta valor. </p>
-        <p>Los artistas exploran, enfrentan desafíos y encuentran nuevas formas de comunicar emociones. Abrazar cada etapa permite un desarrollo auténtico y conexión profunda con la obra. Valorar la travesía creativa fomenta la experimentación, autenticidad y resultados inesperados, haciendo del proceso musical una experiencia rica y satisfactoria. </p>
-        <div className={style.phrase}>
-          <Image
-            className={style.quotesUp}
-            src="/images/quotes-up.svg"
-            alt="comillas hacia arriba"
-            width={25}
-            height={18}
-          />
-          <h4>
-            Encontrá tu <span>IDENTIDAD</span>
-          </h4>
-          <Image
-            className={style.quotesDown}
-            src="/images/quotes-down.svg"
-            alt="comillas hacia abajo"
-            width={25}
-            height={18}
-          />
+        <div>
+          <p>En la producción musical, el viaje creativo es tan crucial como el resultado final. Aunque alcanzar un resultado excepcional es el objetivo, el proceso es donde se despliega la pasión y la expresión artística. Cada fase, desde la concepción hasta la ejecución, aporta valor. </p>
+          <p>Los artistas exploran, enfrentan desafíos y encuentran nuevas formas de comunicar emociones. Abrazar cada etapa permite un desarrollo auténtico y conexión profunda con la obra. Valorar la travesía creativa fomenta la experimentación, autenticidad y resultados inesperados, haciendo del proceso musical una experiencia rica y satisfactoria. </p>
+          <div className={style.phrase}>
+            <Image
+              className={style.quotesUpMobile}
+              src="/images/quotes-up.svg"
+              alt="comillas hacia arriba"
+              width={25}
+              height={18}
+            />
+            <Image
+              className={style.quotesUpDesktop}
+              src="/images/quotes-up.svg"
+              alt="comillas hacia arriba"
+              width={50}
+              height={35}
+            />
+            <h4>
+              Encontrá tu <span>IDENTIDAD</span>
+            </h4>
+            <Image
+              className={style.quotesDownMobile}
+              src="/images/quotes-down.svg"
+              alt="comillas hacia abajo"
+              width={25}
+              height={18}
+            />
+            <Image
+              className={style.quotesDownDesktop}
+              src="/images/quotes-down.svg"
+              alt="comillas hacia abajo"
+              width={50}
+              height={35}
+            />
+          </div>
         </div>
         <iframe src="https://open.spotify.com/embed/album/4rB6AFiDR9fLs3kokOzv8q?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 
--- src/app/ui/recordingStudio/RecordingStudio.jsx
+++ src/app/ui/recordingStudio/RecordingStudio.jsx
@@ -8,26 +8,44 @@ export default function RecordingStudio() {
     <section id="recordingStudio" className={style.recordingStudio}>
       <SectionTitle title={"Estudio de grabación"} />
       <article>
-        <p>Bienvenido al emocionante mundo de la grabación musical en nuestro estudio de vanguardia. Ofrecemos un servicio integral, desde la concepción de ideas hasta la ejecución meticulosa, en un entorno creativo donde músicos, productores y artistas colaboran para explorar nuevas dimensiones sonoras. </p>
-        <p>Con equipos de última generación y un equipo apasionado, nos comprometemos a elevar la calidad de tu grabación, valorando cada fase del proceso creativo. Descubre cómo nuestro estudio no solo captura el sonido, sino que también potencia la expresión artística, convirtiendo cada proyecto en una experiencia musical enriquecedora y memorable. Confía en nosotros para llevar tu música al siguiente nivel, donde la excelencia y la creatividad se fusionan.</p>
-        <div className={style.phrase}>
-          <Image
-            className={style.quotesUp}
-            src="/images/quotes-up.svg"
-            alt="comillas hacia arriba"
-            width={25}
-            height={18}
-          />
-          <h4>
-            Encontrá tu <span>SONIDO</span>
-          </h4>
-          <Image
-            className={style.quotesDown}
-            src="/images/quotes-down.svg"
-            alt="comillas hacia abajo"
-            width={25}
-            height={18}
-          />
+        <div className={style.rscontainer}>
+          <div>
+            <p>Bienvenido al emocionante mundo de la grabación musical en nuestro estudio de vanguardia. Ofrecemos un servicio integral, desde la concepción de ideas hasta la ejecución meticulosa, en un entorno creativo donde músicos, productores y artistas colaboran para explorar nuevas dimensiones sonoras. </p>
+            <p>Con equipos de última generación y un equipo apasionado, nos comprometemos a elevar la calidad de tu grabación, valorando cada fase del proceso creativo. Descubre cómo nuestro estudio no solo captura el sonido, sino que también potencia la expresión artística, convirtiendo cada proyecto en una experiencia musical enriquecedora y memorable. Confía en nosotros para llevar tu música al siguiente nivel, donde la excelencia y la creatividad se fusionan.</p>
+          </div>
+          <div className={style.phrase}>
+            <Image
+              className={style.quotesUpMobile}
+              src="/images/quotes-up.svg"
+              alt="comillas hacia arriba"
+              width={25}
+              height={18}
+            />
+            <Image
+              className={style.quotesUpDesktop}
+              src="/images/quotes-up.svg"
+              alt="comillas hacia arriba"
+              width={50}
+              height={35}
+            />
+            <h4>
+              Encontrá tu <span>Sonido</span>
+            </h4>
+            <Image
+              className={style.quotesDownMobile}
+              src="/images/quotes-down.svg"
+              alt="comillas hacia abajo"
+              width={25}
+              height={18}
+            />
+            <Image
+              className={style.quotesDownDesktop}
+              src="/images/quotes-down.svg"
+              alt="comillas hacia abajo"
+              width={50}
+              height={35}
+            />
+          </div>
         </div>
         <iframe src="https://open.spotify.com/embed/album/5ZelHaf1LMhBIPlN26mVFJ?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
       </article>
