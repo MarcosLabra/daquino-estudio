0add8fa23ca13baf7f8bc90f4274130716ca027b
MarcosLabra <mlabra.dev@gmail.com>
2024-01-09 16:49:18 -0300

    termino responsive net, solo falta carousel de fotos

--- src/app/ui/footer/footer.jsx
+++ src/app/ui/footer/footer.jsx
@@ -7,8 +7,10 @@ import Copyright from './Copyright';
 export default function Footer() {
     return (
         <footer id='contact' className={style.footer}>
+        <div className={style.container}>
             <ContactForm />
             <Info />
+        </div>
             <Copyright />
         </footer>
     )
