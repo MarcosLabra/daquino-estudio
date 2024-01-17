c4bde6557c0dfb72223c188069c0696dd406e25c
MarcosLabra <mlabra.dev@gmail.com>
2024-01-17 20:16:58 -0300

    corrijo error de video

--- src/app/ui/session/Video.jsx
+++ src/app/ui/session/Video.jsx
@@ -1,34 +1,22 @@
 "use client"
 
-import { useState, useEffect } from "react";
 import style from "./video.module.scss";
 
 export default function Video() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <div className={style.video}>
-      {windowWidth > 854 ? (
-        <video controls>
-          <source src={"/videos/video1080lite.mp4"} type="video/mp4" />
-          Tu navegador no soporta el tag de video.
-        </video>
-      ) : (
-        <video controls>
-          <source src={"/videos/video480lite.mp4"} type="video/mp4" />
-          Tu navegador no soporta el tag de video.
-        </video>
-      )}
+
+      <video controls className={style.videoMobile}>
+        <source src={"/videos/video480lite.mp4"} type="video/mp4" />
+        Tu navegador no soporta el tag de video.
+      </video>
+
+      <video className={style.videoDesktop} controls>
+        <source src={"/videos/video1080lite.mp4"} type="video/mp4" />
+        Tu navegador no soporta el tag de video.
+      </video>
+
     </div>
   );
 }
