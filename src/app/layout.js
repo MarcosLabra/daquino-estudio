d2f7af697c7afb6e1874019becefb4234fe4c354
MarcosLabra <mlabra@gmail.com>
2024-07-29 14:12:54 -0300

    agrego seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,12 +12,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <Head>
+    <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <meta name="keywords" content="Estudio, Producción, Musical, Grabación, Músicos, sesión" />
+        <meta name="keywords" content="Estudio, Producción, Musical, Grabación, Músicos, Sesión, Mar del Plata, Mezcla, Masterización" />
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Daquino Estudio" />
+        <meta name="geo.region" content="AR-B" />
+        <meta name="geo.placename" content="Mar del Plata" />
+        <meta name="geo.position" content="-38.0000;-57.5500" />
+        <meta name="ICBM" content="-38.0000, -57.5500" />
       </Head>
       <body className={manrope.className}>
         <Navbar />
